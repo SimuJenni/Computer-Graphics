414ff834231e80d4551eb30769884bc3787a7cea
Simon Jenni <simujenni@students.unibe.ch>
2016-03-11 23:48:45 +0100

    Cleanup

--- hw2-osx-10.9/Build/Intermediates/hw2.build/Debug/hw2.build/Objects-normal/x86_64/grader.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/simujenni/Computer-Graphics/hw2-osx-10.9/UCB/grader.cpp \
-  /Users/simujenni/Computer-Graphics/hw2-osx-10.9/UCB/grader.h
--- hw2-osx-10.9/Build/Intermediates/hw2.build/Debug/hw2.build/Objects-normal/x86_64/shaders.d
+++
@@ -1,1 +0,0 @@
-dependencies: /Users/simujenni/Computer-Graphics/hw2-osx-10.9/shaders.cpp

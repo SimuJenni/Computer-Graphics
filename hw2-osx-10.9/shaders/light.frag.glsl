f6f9b2d09e8151d46968df3e07fb11e71b5f9d26
Simon Jenni <simujenni@students.unibe.ch>
2016-03-12 12:44:46 +0100

    Shading implemented. Should be more or less fine, can't seem to verify however since the grading tool seems to be down.

--- hw2-osx-10.9/shaders/light.frag.glsl
+++ hw2-osx-10.9/shaders/light.frag.glsl
@@ -29,18 +29,54 @@ uniform vec4 ambient;
 uniform vec4 diffuse; 
 uniform vec4 specular; 
 uniform vec4 emission; 
-uniform float shininess; 
+uniform float shininess;
+
+vec4 ComputeLight (const in vec3 direction, const in vec4 lightcolor, const in vec3 normal, const in vec3 halfvec, const in vec4 mydiffuse, const in vec4 myspecular, const in float myshininess) {
+    
+    float nDotL = dot(normal, direction)  ;
+    vec4 lambert = mydiffuse * lightcolor * max (nDotL, 0.0) ;
+    
+    float nDotH = dot(normal, halfvec) ;
+    vec4 phong = myspecular * lightcolor * pow (max(nDotH, 0.0), myshininess) ;
+    
+    vec4 retval = lambert + phong ;
+    return retval ;
+}
 
 void main (void) 
 {       
-    if (enablelighting) {       
-        vec4 finalcolor;
-        // YOUR CODE FOR HW 2 HERE
-        // A key part is implementation of the fragment shader
-        vec4 lighDir = normalize(myvertex - lightposn[0]);
-        float nDotL = dot(lighDir.xyz, mynormal);
-        finalcolor = ambient+nDotL*diffuse;
+    if (enablelighting) {
+        vec4 finalcolor = vec4(0,0,0,0);
+        
+        const vec3 eyepos = vec3(0,0,0) ;
+        vec4 _mypos = gl_ModelViewMatrix * myvertex ;
+        vec3 mypos = _mypos.xyz / _mypos.w ; // Dehomogenize current location
+        vec3 eyedirn = normalize(eyepos - mypos) ;
+        
+        // Compute normal, needed for shading.
+        // Simpler is vec3 normal = normalize(gl_NormalMatrix * mynormal) ;
+        vec3 _normal = (gl_ModelViewMatrixInverseTranspose*vec4(mynormal,0.0)).xyz ;
+        vec3 normal = normalize(_normal) ;
+        
+        for (int i=0; i<numused; i++) {
+            if (abs(lightposn[i].w)>0.01) {
+                // Handle point lights
+                vec4 lightpos = gl_ModelViewMatrix * lightposn[i];
+                lightpos = lightpos/lightpos.w;
+                vec3 lighDir = normalize(vec3(lightpos) - mypos);
+                vec3 halfvec = normalize(lighDir + eyedirn) ;
+                vec4 col = ComputeLight(lighDir, lightcolor[i], normal, halfvec, diffuse, specular, shininess) ;
+                finalcolor = finalcolor + col;
+            } else {
+                // Handle directional lights
+                vec3 direction = normalize (lightposn[i].xyz) ;
+                vec3 halfvec = normalize (direction + eyedirn) ;
+                vec4 col = ComputeLight(direction, lightcolor[i], normal, halfvec, diffuse, specular, shininess);
+                finalcolor = finalcolor + col;
+            }
+        }
 
+        finalcolor = finalcolor + ambient;
         gl_FragColor = finalcolor;
     } else {
         gl_FragColor = color; 
